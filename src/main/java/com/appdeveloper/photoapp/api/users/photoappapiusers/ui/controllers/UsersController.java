1aed4a16c67cf59a95f7de641a7b38f28a165f22
Gustavonvp <reinaldo.nunes@mezzow.com>
2021-11-19 20:05:34 -0300

    Post Route. Closes #11

--- src/main/java/com/appdeveloper/photoapp/api/users/photoappapiusers/ui/controllers/UsersController.java
+++ src/main/java/com/appdeveloper/photoapp/api/users/photoappapiusers/ui/controllers/UsersController.java
@@ -1,15 +1,24 @@
 package com.appdeveloper.photoapp.api.users.photoappapiusers.ui.controllers;
 
+import com.appdeveloper.photoapp.api.users.photoappapiusers.ui.rest.model.requests.CreateUserRequestModel;
+import com.appdeveloper.photoapp.api.users.photoappapiusers.ui.rest.shared.UserDto;
+import com.appdeveloper.photoapp.api.users.photoappapiusers.ui.services.UsersService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
 public class UsersController {
 
     private final Environment env;
+
+    @Autowired
+    UsersService usersService;
 
     public UsersController(Environment env) {
         this.env = env;
@@ -20,5 +29,17 @@ public class UsersController {
         return
 
                 "Working on port:" + env.getProperty("local.server.port");
+    }
+
+
+    @PostMapping
+    public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = modelMapper.map(userDetails, UserDto.class);
+        usersService.createUser(userDto);
+        return "Create user method is called";
     }
 }
