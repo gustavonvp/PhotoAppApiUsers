045a50eb3955e6e39b940106ec8194c59eacf7ea
Gustavonvp <reinaldo.nunes@mezzow.com>
2021-11-21 22:03:00 -0300

    Using permit all. . Closes #15

--- src/main/java/com/appdeveloper/photoapp/api/users/photoappapiusers/ui/security/WebSecurity.java
+++ src/main/java/com/appdeveloper/photoapp/api/users/photoappapiusers/ui/security/WebSecurity.java
@@ -1,18 +1,30 @@
 package com.appdeveloper.photoapp.api.users.photoappapiusers.ui.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
+    private Environment environment;
+
+    @Autowired
+    public WebSecurity(Environment environment) {
+        this.environment = environment;
+    }
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeHttpRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/users/**").permitAll();
         http.headers().frameOptions().disable();
     }
 }
